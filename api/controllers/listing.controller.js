a64faf42b70d4e15b1a3dad988b4f412209a43b8
suraj <kumarsurajverma6001@gmail.com>
2024-02-07 23:08:24 +0530

    add search functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -53,17 +53,17 @@ export const getListings = async (req, res, next) => {
 
     let offer = req.query.offer;
     if (offer === undefined || offer === "false") {
-      offer = { $in: [falase, true] };
+      offer = false; // Corrected to use boolean value
     }
 
     let furnished = req.query.furnished;
     if (furnished === undefined || furnished === "false") {
-      furnished = { $in: [falase, true] };
+      furnished = false; // Corrected to use boolean value
     }
 
     let parking = req.query.parking;
     if (parking === undefined || parking === "false") {
-      parking = { $in: [falase, true] };
+      parking = false; // Corrected to use boolean value
     }
 
     let type = req.query.type;
@@ -71,14 +71,14 @@ export const getListings = async (req, res, next) => {
       type = { $in: ["rent", "sale"] };
     }
 
-    const searchTeam = req.query.searchTeam || "";
+    const searchTerm = req.query.searchTerm || "";
 
     const sort = req.query.sort || "createdAt";
 
     const order = req.query.order || "desc";
 
-    const listing = await Listing.find({
-      name: { $regex: searchTeam, $options: "i" },
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
       offer,
       furnished,
       parking,
@@ -88,8 +88,9 @@ export const getListings = async (req, res, next) => {
       .limit(limit)
       .skip(startIndex);
 
-    return res.status(200).json(listing);
+    return res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
 };
+
