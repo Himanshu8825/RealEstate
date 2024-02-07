ece02522b3f1d18da8c2f254f77077673089990e
suraj <kumarsurajverma6001@gmail.com>
2024-02-08 01:40:08 +0530

    create about page and home page

--- api/index.js
+++ api/index.js
@@ -1,46 +1,50 @@
 import express from "express";
 const app = express();
 import mongoose from "mongoose";
-import 'dotenv/config';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js'; 
-import cookieParser from 'cookie-parser'
+import "dotenv/config";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 
-const port = process.env.PORT ;
+const port = process.env.PORT;
 const dbUrl = process.env.MONGO_DB_URI;
 
 mongoose
-    .connect(dbUrl)
+  .connect(dbUrl)
 
-    .then(() => {
-        console.log('DB Connected');
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    })
-})
-
-
-
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(port, (req, res) => {
-    console.log(`Server started at port ${port}`);
-})
+  console.log(`Server started at port ${port}`);
+});
