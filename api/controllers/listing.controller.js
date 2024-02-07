c307720e1caeee23cb3354110c2fd6df4716fe22
suraj <kumarsurajverma6001@gmail.com>
2024-02-08 00:50:35 +0530

    created a listingItem and show listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -46,29 +46,45 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
 export const getListings = async (req, res, next) => {
   try {
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
-
     let offer = req.query.offer;
+
     if (offer === undefined || offer === "false") {
-      offer = false; // Corrected to use boolean value
+      offer = { $in: [false, true] };
     }
 
     let furnished = req.query.furnished;
+
     if (furnished === undefined || furnished === "false") {
-      furnished = false; // Corrected to use boolean value
+      furnished = { $in: [false, true] };
     }
 
     let parking = req.query.parking;
+
     if (parking === undefined || parking === "false") {
-      parking = false; // Corrected to use boolean value
+      parking = { $in: [false, true] };
     }
 
     let type = req.query.type;
+
     if (type === undefined || type === "all") {
-      type = { $in: ["rent", "sale"] };
+      type = { $in: ["sale", "rent"] };
     }
 
     const searchTerm = req.query.searchTerm || "";
